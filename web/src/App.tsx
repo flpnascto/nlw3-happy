ef558a1f9d7786ba163f1819e93b931740ad480b
Felipe Nascimento <flpnascto@gmail.com>
2020-10-13 16:20:36 -0300

    Criando conteúdo da página incial.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css'
+
+import logoImg from '../src/images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper"></div>
+        <img src={logoImg} alt="Happy"></img>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Belo Horizonte</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <a href="" className="enter-app">
+          enter
+        </a>
     </div>
   );
 }
