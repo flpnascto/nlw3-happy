22adf66528fae25ce12aae0da49cafa0a35f8d6d
Felipe Nascimento <flpnascto@gmail.com>
2020-10-13 18:48:30 -0300

    Criando conteúdo da página Orphanage Map.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi'
+
+import mapMarketImg from '../images/map-marker.svg';
 
 function OrphanagesMap() {
   return (
-    <h1>Orphanages Map page</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarketImg} alt="mapMarker"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Belo Horizonte</strong>
+          <span>Minas Gerais</span>
+        </footer>
+
+      </aside>
+
+      <div>
+        {/* div for map */}
+      </div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+
+    </div>
   );
 }
 
