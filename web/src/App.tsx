14d43cf25b2342007798852432d8cb1089bb5d4f
Felipe Nascimento <flpnascto@gmail.com>
2020-10-13 17:37:04 -0300

    Estilizando página incial.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
 
 import './styles/global.css';
 import './styles/pages/landing.css'
@@ -8,8 +9,9 @@ import logoImg from '../src/images/logo.svg'
 function App() {
   return (
     <div id="page-landing">
-      <div className="content-wrapper"></div>
-        <img src={logoImg} alt="Happy"></img>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
@@ -21,8 +23,11 @@ function App() {
         </div>
 
         <a href="" className="enter-app">
-          enter
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
         </a>
+
+      </div>
+  
     </div>
   );
 }
