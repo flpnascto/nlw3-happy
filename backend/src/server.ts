49b788b434f2abc74bf1fe3fe619136c5faa4105
Felipe Nascimento <flpnascto@gmail.com>
2020-10-15 19:44:46 -0300

    Lidando com exceções. ErrorHandler.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors'
 
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './erros/handler';
 
 const app = express();
 
@@ -11,4 +13,6 @@ app.use(express.json());
 
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
+
 app.listen(3333);
