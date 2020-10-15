2fd9ea52e16861740859a785bbb20fdd18fb10f6
Felipe Nascimento <flpnascto@gmail.com>
2020-10-15 20:04:38 -0300

    Adicionando validação de dados para inserção do orfanato. Adicionando CORS.

--- backend/src/controllers/OrphanagesCotroller.ts
+++ backend/src/controllers/OrphanagesCotroller.ts
@@ -3,6 +3,7 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import Orphanages from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view'
+import * as Yup from 'yup';
 
 export default {
   async index(request: Request, response:Response) {
@@ -45,8 +46,8 @@ export default {
       const images = requestImages.map(image => {
         return { path: image.filename}
       })
-      
-      const orphanage = orphanagesRepository.create({
+
+      const data = {
         name,
         latitude,
         longitude,
@@ -55,9 +56,31 @@ export default {
         opening_hours,
         open_on_weekends,
         images
-        });
+      };
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        latitude: Yup.number().required(),
+        longitude: Yup.number().required(),
+        about: Yup.string().required().max(300),
+        instructions: Yup.string().required(),
+        opening_hours: Yup.string().required(),
+        open_on_weekends: Yup.boolean().required(),
+        images: Yup.array(
+          Yup.object().shape({
+            path: Yup.string().required()
+        })
+        )
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      
+      const orphanage = orphanagesRepository.create(data);
   
-        await orphanagesRepository.save(orphanage);
+      await orphanagesRepository.save(orphanage);
   
       return response.status(201).json(orphanage);
   }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import 'express-async-errors'
+import cors from 'cors';
 
 import './database/connection';
 
@@ -9,8 +10,8 @@ import errorHandler from './erros/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler);
