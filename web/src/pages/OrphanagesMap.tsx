065324a5bb158b5632e0ed5967f5895c5ef5d77b
Felipe Nascimento <flpnascto@gmail.com>
2020-10-16 11:06:38 -0300

    Ajuste para exibir os orfanatos cadastrados no mapa.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
 import '../styles/pages/orphanages-map.css';
 
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
 function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, [])
+
   return (
     <div id="page-map">
       <aside>
@@ -26,24 +42,31 @@ function OrphanagesMap() {
       </aside>
 
       <Map
-        center={[-19.9262, -43.9383]}
-        zoom={12.75}
+        // center={[-19.9262, -43.9383]}
+        // zoom={12.75}
+        center={[-27.2092052,-49.6401092]}
+        zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        {orphanages.map(orphanage => {
+          return (
+            <Marker
+              key={orphanage.id}
+              icon={mapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+            >
+                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                  {orphanage.name}
+                  <Link to={`/orphanages/${orphanage.id}`}>
+                    <FiArrowRight size={20} color="#FFF" />
+                  </Link>
 
-        <Marker
-          icon={mapIcon}
-          position={[-19.9262, -43.9383]}
-          >
-            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-              Lar da meninas
-              <Link to="/orphanages/1">
-                <FiArrowRight size={20} color="#FFF" />
-              </Link>
+                </Popup>
+            </Marker>
+          );
+        })}
 
-            </Popup>
-        </Marker>
       </Map>
 
       <Link to="/orphanages/create" className="create-orphanage">
