c479f4d6dce6f4dc118558ca508c5769fbe57f97
Felipe Nascimento <flpnascto@gmail.com>
2020-10-16 08:24:47 -0300

    Finalizando página de mapa.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
 
-import mapMarketImg from '../images/map-marker.svg';
+import mapMarkerImg from '../images/map-marker.svg';
 
-import '../styles/pages/orphanages-map.css'
+import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor:[29, 68],
+  popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarketImg} alt="mapMarker"/>
+          <img src={mapMarkerImg} alt="mapMarker"/>
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças estão esperando a sua visita :)</p>
         </header>
@@ -32,9 +40,22 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+
+        <Marker
+          icon={mapIcon}
+          position={[-19.9262, -43.9383]}
+          >
+            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+              Lar da meninas
+              <Link to="/orphanages/1">
+                <FiArrowRight size={20} color="#FFF" />
+              </Link>
+
+            </Popup>
+        </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
 
